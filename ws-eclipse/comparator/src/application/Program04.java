b9ce6c7616849e652bd9826ad5085d84a7d775e3
Giovany Almeida <gvncesar@gmail.com>
2025-09-05 17:47:29 -0300

    expressão lambda

---
+++ ws-eclipse/comparator/src/application/Program04.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		// resumindo classe anônima com expressão lambda
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+		list.sort(comp);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+
+}
