3a485e2a84b62edf605ff22c927f643be3dd0cc2
Giovany Almeida <gvncesar@gmail.com>
2025-09-19 18:59:58 -0300

    JDBC

--- ws-eclipse/jdbcSELECT/src/application/Program.java
+++ ws-eclipse/jdbcSELECT/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 
 			st = conn.createStatement();
 
-			rs = st.executeQuery("select * from department;");
+			rs = st.executeQuery("SELECT * FROM department;");
 
 			while (rs.next()) {
 				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
---
+++ ws-eclipse/jdbcUPDATE/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module jdbc01 {
+	requires java.sql;
+    requires mysql.connector.j; // nome do módulo do MySQL
+}
